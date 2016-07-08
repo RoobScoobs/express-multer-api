6c6df5dd92088a221e4f83ae7f7c9733c320566c
RoobScoobs <iruben229@gmail.com>
2016-07-08 15:28:51 -0400

    Refactor into upload module
    This module is usable from both a command line application and an express
    api server

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -3,37 +3,8 @@
 require('dotenv').config();
 
 const fs = require('fs');
-const crypto = require('crypto');
 
-const fileType = require('file-type');
-const AWS = require('aws-sdk');
-
-const s3 = new AWS.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  }
-});
-
-const mimeType = (data) => {
-  // thing on the right gets added, or rather overwrites, the thing on the left
-  // Object.assign({}, {}) ~ hash.merge in Ruby
-  return Object.assign({
-    ext: 'bin',
-    mime: 'application/octet-stream'
-  }, fileType(data));
-};
-
-const randomHexString = (length) => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (error, buffer) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(buffer.toString('hex'));
-    });
-  });
-};
+const uploader = require('../lib/aws-s3-upload');
 
 let filename = process.argv[2] || ''; // <-- '' Adds a default (empty strings) if no argument is passed
 
@@ -49,40 +20,9 @@ const readFile = (filename) => {
   });
 };
 
-const awsUpload = (file) => {
-  return randomHexString(16)
-  .then((filename) => {
-    let dir = new Date().toISOString().split('T')[0];
-    return {
-      ACL: "public-read",
-      Body: file.data,
-      Bucket: 'roobsbucket',
-      ContentType: file.mime,
-      Key: `${dir}/${filename}.${file.ext}`
-    };
-  })
-  .then((options) => {
-    return new Promise((resolve, reject) => {
-      s3.upload(options, (error, data) => {
-        if (error) {
-          reject(error);
-        }
-        resolve(data);
-      });
-    });
-  });
-};
 
-// place .then and .catch outside of the function, so we can see promise or error that is returned
 readFile(filename)
-// Need to build a custom file object bc it is something that AWS expects
-// Aso needs to include an extension and a mime
-.then((data) => {
-  let file = mimeType(data);
-  // attach the buffer to a key named data
-  file.data = data;
-  return file;
-})
-.then(awsUpload)
+.then(uploader.prepareFile)
+.then(uploader.awsUpload)
 .then(console.log)
 .catch(console.error);
